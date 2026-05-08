9068c4bbacce9fc9fac70fc949c12766cc0b8548
MahatTapas <tapas.mahat123@gmail.com>
2026-05-08 23:29:25 +0545

    fix: fixed the response type and also modified the timezone conversion

--- scripts/extract_steam.py
+++ scripts/extract_steam.py
@@ -2,21 +2,21 @@
 import requests
 import json
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 URL="https://store.steampowered.com/api/featuredcategories/"
 RAW_PATH = "data/raw/steam_raw.json"
 
 def fetch_steam_data():
     response = requests.get(URL)
-    respone.raise_for_status() # instead of doint it manually where we do if response.status_code ==200 we use this, this automaticlly throws an exception and is much suitable for airflow, since the manual way executes either way the airflow takes it as it ran successfully but with this way it will fail if the status code is not 200
+    response.raise_for_status() # instead of doint it manually where we do if response.status_code ==200 we use this, this automaticlly throws an exception and is much suitable for airflow, since the manual way executes either way the airflow takes it as it ran successfully but with this way it will fail if the status code is not 200
     return response.json()
 
 def save_raw_data(data):
     os.makedirs("data/raw", exist_ok=True)
     with open(RAW_PATH,'w') as f:
         json.dump({
-            "fetched_at":datetime.now(nepal_tz).isoformat(),"timezone":"GMT+5:45",
+            "fetched_at":datetime.now(timezone(timedelta(hours=5, minutes=45))).isoformat(),"timezone":"GMT+5:45",
             "data": data
             }, f, indent=4)
         
