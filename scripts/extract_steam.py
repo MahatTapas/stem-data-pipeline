71ce3ec87e88298ebc5802612670028592afb5bf
MahatTapas <tapas.mahat123@gmail.com>
2026-05-08 23:18:58 +0545

    fix: url bug fixed

---
+++ scripts/extract_steam.py
@@ -0,0 +1,27 @@
+#in this script we extract the api from twitter
+import requests
+import json
+import os
+from datetime import datetime, timezone
+
+URL="https://store.steampowered.com/api/featuredcategories/"
+RAW_PATH = "data/raw/steam_raw.json"
+
+def fetch_steam_data():
+    response = requests.get(URL)
+    respone.raise_for_status() # instead of doint it manually where we do if response.status_code ==200 we use this, this automaticlly throws an exception and is much suitable for airflow, since the manual way executes either way the airflow takes it as it ran successfully but with this way it will fail if the status code is not 200
+    return response.json()
+
+def save_raw_data(data):
+    os.makedirs("data/raw", exist_ok=True)
+    with open(RAW_PATH,'w') as f:
+        json.dump({
+            "fetched_at":datetime.now(nepal_tz).isoformat(),"timezone":"GMT+5:45",
+            "data": data
+            }, f, indent=4)
+        
+
+if __name__ == "__main__":
+    data = fetch_steam_data()
+    save_raw_data(data)
+    print("Data has been fetched and saved successfully.")
