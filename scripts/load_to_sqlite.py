091a6f38f18f8f16a41e6f3013ce6a787c5173d2
MahatTapas <tapas.mahat123@gmail.com>
2026-05-09 00:20:59 +0545

    Add: created a scripe that loads data in the database

---
+++ scripts/load_to_sqlite.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas as pd
+
+DB_PATH = "db/steam.db"
+CSV_PATH = "data/processed/games.csv"
+
+def load_data():
+    conn = sqlite3.connect(DB_PATH)
+    df = pd.read_csv(CSV_PATH)
+    df.to_sql("raw_games", conn, if_exists="append", index=False)
+    conn.close()
+
+if __name__ == "__main__":
+    load_data()
+    print("Data has been loaded into SQLite successfully.")
